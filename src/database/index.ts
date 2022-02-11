078a03191c96c8e1f78364bfd7a6e4b1ce68a0d1
Pedro <russopedro87@gmail.com>
2022-02-11 19:13:56 -0300

    adding config for postgres prod and local envs

---
+++ src/database/index.ts
@@ -0,0 +1,24 @@
+import { createConnection } from "typeorm";
+
+require("dotenv").config();
+
+const config: any = {
+  type: "postgres",
+  //url: process.env.DATABASE_URL,
+  host: process.env.TYPEORM_HOST,
+  port: process.env.TYPEORM_PORT || 5432,
+  username: process.env.TYPEORM_USERNAME || "postgres",
+  password: process.env.TYPEORM_PASSWORD || "root",
+  database: process.env.TYPEORM_DATABASE || "tasks",
+  synchronize: process.env.TYPEORM_SYNCHRONIZE,
+
+  entities: ["src/entity/*.ts"],
+  migrations: ["src/migration/**/*.ts"],
+  cli: {
+    entitiesDir: "src/entity",
+    migrationsDir: "src/migration",
+    subscribersDir: "src/subscriber",
+  },
+};
+
+createConnection(config).catch((error) => console.log(error));
--- src/index.ts
+++ src/index.ts
@@ -3,24 +3,20 @@ import { createConnection } from "typeorm";
 const express = require("express");
 const cors = require("cors");
 const helmet = require("helmet");
-
+require("dotenv").config();
 import routes from "./routes";
+import "./database";
 
 const PORT = process.env.PORT || 3000;
 
-createConnection()
-  .then(async () => {
-    // create express app
-    const app = express();
+const app = express();
 
-    app.use(cors());
-    app.use(helmet());
-    app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(express.json());
 
-    app.use("/", routes);
+app.use("/", routes);
 
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((error) => console.log(error));
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
